f92820053c01466457b8051fb18f4422b723652d
Tricia-Kaneria <tricia.kaneria@hotmail.co.uk>
2024-09-07 23:36:46 +0100

    Add project files.

---
+++ CustomerApp/Controllers/CustomerController.cs
@@ -0,0 +1,68 @@
+﻿using CustomerApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly APIGateway apiGateway;
+        public CustomerController(APIGateway ApiGateway)
+        {
+            this.apiGateway = ApiGateway;
+        }
+        public IActionResult Index()
+        {
+            List<Customer> customers = apiGateway.ListCustomers();
+
+            return View(customers);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            Customer customer = new Customer();
+
+            return View(customer);
+        }
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            apiGateway.CreateCustomer(customer);
+
+            return RedirectToAction("index");
+        }
+        public IActionResult Details(int Id)
+        {
+            Customer customer = apiGateway.GetCustomer(Id);
+
+            return View(customer);
+        }
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            Customer customer = apiGateway.GetCustomer(Id);
+
+            return View(customer);
+        }
+        [HttpPost]
+        public IActionResult Edit(Customer customer)
+        {
+            apiGateway.UpdateCustomer(customer);
+
+            return RedirectToAction("index");
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Customer customer = apiGateway.GetCustomer(id);
+
+            return View(customer);
+        }
+        [HttpPost]
+        public IActionResult Delete(Customer customer)
+        {
+            apiGateway.DeleteCustomer(customer.id);
+
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ CustomerApp/Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace CustomerApp.Models
+{
+    public class Customer
+    {
+        [Key]
+        [Display(Name = "Id")]
+        public int id { get; set; }
+
+        [StringLength(75)]
+        [Display(Name = "First Name")]
+        [Required]
+        public string firstName { get; set; } = "";
+
+        [StringLength(75)]
+        [Display(Name = "Last Name")]
+        [Required]
+        public string lastName { get; set; } = "";
+
+        [StringLength(50)]
+        [Display(Name = "Tel No.")]
+        [Required]
+        public string phoneNo { get; set; } = "";
+
+        [StringLength(75)]
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [Required]
+        public string email { get; set; } = "";
+    }
+}
